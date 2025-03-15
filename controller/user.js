d983bb838f208107dbc68ace89131581c63c9578
choin <choin0330@gmail.com>
2025-03-15 21:39:50 +0900

    feat
    검사 항목 설정 기능 추가

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 import * as userData from "../data/user.js";
 
-// 사용자 검사 항목 설정 조회
+// 특정 사용자 검사 항목 설정 조회
 export const getCheckSettings = async (req, res, next) => {
     try {
         const { username } = req.params;
@@ -17,6 +17,38 @@ export const getCheckSettings = async (req, res, next) => {
         return res.status(200).json({
             success: true,
             data: user.checkCategory
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// 특정 사용자 검사 항목 저장(또는 수정)
+export const setCheckSettings = async (req, res, next) => {
+    try {
+        const { username } = req.params;
+        const { risk, brand, it, foodDrink } = req.body;
+
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "요청 데이터가 올바르지 않습니다"
+            });
+        }
+
+        // 업데이트할 필드만 포함
+        const updatedUser = await userData.updateUserCheckSettings(username, { risk, brand, it, foodDrink });
+
+        if (!updatedUser) {
+            return res.status(404).json({
+                success: false,
+                message: "해당 유저를 찾을 수 없습니다"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "설정이 성공적으로 저장되었습니다"
         });
     } catch (error) {
         next(error);
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,7 @@ const router = express.Router();
 // 검사 항목 설정 조회
 router.get("/setcheck/:username", userController.getCheckSettings);
 
+// 검사 항목 설정
+router.put("/setcheck/:username", userController.setCheckSettings)
+
 export default router;
